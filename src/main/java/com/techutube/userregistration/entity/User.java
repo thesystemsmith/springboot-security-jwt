1a2309c0a88c29ce856bfcfdc8eb0abf6a3f0d6c
thesystemsmith <abdulrasheed.techy@gmail.com>
2024-07-21 01:26:45 +0530

    changing the generation strategy

--- src/main/java/com/techutube/userregistration/entity/User.java
+++ src/main/java/com/techutube/userregistration/entity/User.java
@@ -17,7 +17,7 @@ import java.util.List;
 @Table(name = "tech_user")
 public class User implements UserDetails {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @Column(unique = true, updatable = false)
     private String email;
